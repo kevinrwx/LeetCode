1b57d0eedd95e59ccc21ae9c2318e53838af718f
chang <lchangrwx@gmail.com>
2015-03-12 21:20:23 +0800

    an awesome way to palindrome number

--- Math/palindromeNumber/main.cpp
+++ Math/palindromeNumber/main.cpp
@@ -6,45 +6,64 @@
 
 using namespace std;
 
-//this is a generic way, but used extra spaces.
-bool isPalindrome(int x)
-{
-    vector<int> tmp;
-    int mod;
-    if(x < 0) {
-        return false;
-    }
-    else {
-        while(x != 0) {
-            mod = x % 10;
-            tmp.push_back(mod);
-            x /= 10;
-        }
-        for(int i = 0, j = tmp.size() - 1; i < j; i++ , j--) {
-            if(tmp[i] != tmp[j])
-                return false;
-        }
-        return true;
-    }
-}
+////this is a generic way, but used extra spaces.
+//bool isPalindrome(int x)
+//{
+//    vector<int> tmp;
+//    int mod;
+//    if(x < 0) {
+//        return false;
+//    }
+//    else {
+//        while(x != 0) {
+//            mod = x % 10;
+//            tmp.push_back(mod);
+//            x /= 10;
+//        }
+//        for(int i = 0, j = tmp.size() - 1; i < j; i++ , j--) {
+//            if(tmp[i] != tmp[j])
+//                return false;
+//        }
+//        return true;
+//    }
+//}
+//
+////This is also a generic way, but it doesn't use extra spaces.
+//bool isPalindrome(int x)
+//{
+//    if(x < 0)
+//        return false;
+//    int div = 1;
+//    while(x/div >= 10)
+//        div *= 10;
+//    while(x > 0) {
+//        int l = x / div;
+//        int r = x % 10;
+//        if(l != r)
+//            return false;
+//        x = x % div / 10;
+//        div /= 100;
+//    }
+//    return true;
+//}
 
-//This is also a generic way, but it doesn't use extra spaces.
+//This is another awesome way!!! That's really cool!!!!!
 bool isPalindrome(int x)
 {
     if(x < 0)
         return false;
-    int div = 1;
-    while(x/div >= 10)
-        div *= 10;
+    int num = x;
+    int rev = 0;
+    int mod;
     while(x > 0) {
-        int l = x / div;
-        int r = x % 10;
-        if(l != r)
-            return false;
-        x = x % div / 10;
-        div /= 100;
+        mod = x % 10;
+        rev = rev * 10 + mod;
+        x /= 10;
     }
-    return true;
+    if(num == rev)
+        return true;
+    else
+        return false;
 }
 
 
