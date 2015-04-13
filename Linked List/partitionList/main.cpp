329e148882d3892b9f1e75b901d8d778e0f4c38d
chang <lchangrwx@gmail.com>
2015-04-13 10:59:12 +0800

    DP and Greedy

--- Linked List/partitionList/main.cpp
+++ Linked List/partitionList/main.cpp
@@ -15,18 +15,28 @@ ListNode *partition(ListNode *head, int x)
 {
     if(head == NULL)
         return NULL;
-    ListNode *p = head;
-    ListNode *pre;
-    ListNode *index = NULL;
-    while(p != NULL) {
-        if(p->val < x) {
-            pre->next = p->next;
-            p->next = index->next;
-            index->next = p;
-            p = pre->next;
+    struct ListNode assist;
+    assist.val = x-1;
+    assist.next = head;
+    ListNode *p = &assist;
+    ListNode *pre = p;
+    ListNode *cur = p->next;
+    ListNode *index = p;
+
+    while(cur != NULL) {
+        if(cur->val < x) {
+            pre->next = cur->next;
+            cur->next = index->next;
+            index->next = cur;
+            cur = pre->next;
             index = index->next;
         }
+        else {
+            cur = cur->next;
+            pre = pre->next;
+        }
     }
+    return p->next;
 }
 
 int main()
