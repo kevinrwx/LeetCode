acc19f041c0438ac7817a1cac700f834f194d6ae
chang <lchangrwx@gmail.com>
2015-04-15 21:07:11 +0800

    single number

--- Bit/singleNumber/main.cpp
+++ Bit/singleNumber/main.cpp
@@ -1,14 +1,24 @@
+
+//single number
+
 #include <iostream>
 
 using namespace std;
 
 int singleNumber(int A[], int n)
 {
-    //
+    int num = 0;
+    if(A == NULL)
+        return 0;
+    for(int i = 0; i < n; i++)
+        num = num ^ A[i];
+    return num;
 }
 
 int main()
 {
-    cout << "Hello world!" << endl;
+    int A[] = {1,1,2,2,3,3,4};
+    int n = sizeof(A)/sizeof(A[0]);
+    cout<<"The number is: "<<singleNumber(A, n)<<endl;
     return 0;
 }
