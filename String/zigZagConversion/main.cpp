4983b62e70b957f412a54a7f81427cfebaf0be38
chang <lchangrwx@gmail.com>
2015-05-16 21:20:20 +0800

    zigzag conversion

--- String/zigZagConversion/main.cpp
+++ String/zigZagConversion/main.cpp
@@ -8,30 +8,39 @@ using namespace std;
 
 string convert(string s, int numRows)
 {
+    string result;
     int len = s.length();
     if(len == 0)
-        return NULL;
+        return result;
+    if(numRows == 1)
+        return s;
     int k = numRows * 2 - 2;
-    int cycle = len / k;
-    int left = len % k;
 
-    string result;
-    for(int i = 1; i <= numRows; i++) {
-        if(i == 1 || i == numRows) {
+    for(int i = 0; i < numRows; i++) {
+        if(i == 0 || i == numRows-1) {
             for(int j = i; j < len; j += k) {
                 result += s[j];
             }
         }
         else {
-            for(int l = i; l < len; )
+            int tmp = -i;
+            for(int l = i; l < len; l += k) {
+                result += s[l];
+                tmp += k;
+                if(tmp < len)
+                    result += s[tmp];
+            }
         }
     }
+    return result;
 }
 
 int main()
 {
-    string str = "PAYPALISHIRING";
-    int numRows = 3;
+//    string str = "PAYPALISHIRING";
+//    int numRows = 3;
+    string str = "A";
+    int numRows = 2;
     string result = convert(str, numRows);
     cout<<"The result is: "<<result;
     return 0;
