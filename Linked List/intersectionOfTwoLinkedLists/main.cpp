09877d2370189f4187f50cc2215317f7bca881b0
chang <lchangrwx@gmail.com>
2015-05-21 21:16:08 +0800

    intersection of two linked lists

---
+++ Linked List/intersectionOfTwoLinkedLists/main.cpp
@@ -0,0 +1,24 @@
+
+//Intersection of Two Linked Lists
+
+#include <iostream>
+
+using namespace std;
+
+struct ListNode
+{
+    int val;
+    ListNode* next;
+    ListNode(int x) : val(x), next(NULL) {}
+};
+
+ListNode* getInterSectionNode(ListNode* headA, ListNode* headB)
+{
+    //
+}
+
+int main()
+{
+    cout << "Hello world!" << endl;
+    return 0;
+}
