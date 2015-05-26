f9f8212eba5f22d9a244fb20d27f2c22ef838466
kevinrwx <lchangrwx@gmail.com>
2015-05-26 17:38:44 +0800

    longest substring without repeating characters

--- String/longestSubstringWithoutRepeatingCharacters/main.cpp
+++ String/longestSubstringWithoutRepeatingCharacters/main.cpp
@@ -2,16 +2,64 @@
 //Longest Substring Without Repeating Characters
 
 #include <iostream>
+#include <string.h>
+#include <cmath>
 
 using namespace std;
 
+//int lengthOfLongestSubstring(string s)
+//{
+//    int length = s.length();
+//    if(length == 0)
+//        return 0;
+//    int count[26];
+//    memset(count, -1, sizeof(count));
+//
+//    int len = 0;
+//    int maxV = 0;
+//    for(int i = 0; i < length; i++, len++) {
+//        if(count[s[i] - 'a'] >= 0) {
+//            i = count[s[i] - 'a'] + 1;
+//            memset(count, -1, sizeof(count));
+//            maxV = max(len, maxV);
+//            len = 0;
+//        }
+//        count[s[i] - 'a'] = i;
+//    }
+//    return max(len, maxV);
+//}
+
 int lengthOfLongestSubstring(string s)
 {
-    //
+    int length = s.length();
+    if(length == 0)
+        return 0;
+    int count[256];
+    memset(count, -1, sizeof(count));
+    int len = 0;
+    int maxV = 0;
+    for(int i = 0; i < length; i++) {
+        if(count[s[i] - ' '] >= 0) {
+            len = max(len, maxV);
+            i = count[s[i] - ' '] + 1;
+            len = 0;
+            memset(count, -1, sizeof(count));
+        }
+        count[s[i] - ' '] = i;
+    }
+    return max(len, maxV);
 }
+
 
 int main()
 {
-    cout << "Hello world!" << endl;
+    string str = "pwwekw";
+    int result = lengthOfLongestSubstring(str);
+    cout<<"The result is: "<<result<<endl;
     return 0;
 }
+
+
+
+
+
