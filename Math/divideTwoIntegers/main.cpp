68be356d952c5cd6ca0741d602a7f8e74756d048
chang <lchangrwx@gmail.com>
2015-05-27 23:01:11 +0800

    divide two integers

--- Math/divideTwoIntegers/main.cpp
+++ Math/divideTwoIntegers/main.cpp
@@ -2,16 +2,49 @@
 //Divide Two Integers
 
 #include <iostream>
+#include <limits.h>
 
 using namespace std;
 
 int divide(int dividend, int divisor)
 {
-    //
+    if(divisor == 0)
+        return INT_MAX;
+    if(dividend == 0)
+        return 0;
+    int counts = 0;
+    if(dividend >0 && divisor > 0) {
+        while(dividend >= 0) {
+            dividend -= divisor;
+            counts++;
+        }
+    }
+    else if(dividend < 0 && divisor < 0) {
+        while(dividend <= 0) {
+            dividend -= divisor;
+            counts++;
+        }
+    }
+    else if(dividend > 0 && divisor < 0) {
+        while(dividend >= 0) {
+            dividend += divisor;
+            counts--;
+        }
+    }
+    else {
+        while(dividend <= 0) {
+            dividend += divisor;
+            counts--;
+        }
+    }
+    return (counts > 0) ? --counts : ++counts;
 }
 
 int main()
 {
-    cout << "Hello world!" << endl;
+    int dividend = 0;
+    int divisor = 9;
+    int result = divide(dividend, divisor);
+    cout<<"The result is: "<<result<<endl;
     return 0;
 }
