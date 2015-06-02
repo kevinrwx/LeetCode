628b8b6fd2ea59cf3d34c1dd32b13f42bc9e6b27
chang <lchangrwx@gmail.com>
2015-06-03 00:26:50 +0800

    first missing positive

--- Array/firstMissingPositive/main.cpp
+++ Array/firstMissingPositive/main.cpp
@@ -35,44 +35,66 @@ void Swap(int *x, int *y) {
     *x = tmp;
 }
 
+//int firstMissingPositive(vector<int> &nums)
+//{
+//    int length = nums.size();
+//    if(length == 0)
+//        return 0;
+//    int counts = 0;
+//    int index = 0;
+//    int tmp = 0;
+//    while(counts < length) {
+//        if(index != nums[index]) {
+//            tmp = nums[index];
+//            if(tmp >= 0 && tmp < length) {
+//                Swap(&nums[index], &nums[tmp]);
+//                index = nums[index];
+//            }
+//            else {
+//                index = ;
+//            }
+//        }
+//        counts++;
+//    }
+//    int i;
+//    for(i = 0; i < length; i++) {
+//        if(nums[i] >= 0 && nums[i] != i)
+//            return i;
+//    }
+//    return i;
+//}
+
 int firstMissingPositive(vector<int> &nums)
 {
     int length = nums.size();
     if(length == 0)
-        return 0;
-    int counts = 0;
-    int index = 0;
-    int tmp = 0;
-    while(counts < length) {
-        if(index != nums[index]) {
-            tmp = nums[index];
-            if(tmp >= 0 && tmp < length) {
-                Swap(&nums[index], &nums[tmp]);
-                index = nums[index];
-            }
-            else {
-                index = ;
-            }
+        return 1;
+    int *tmp = new int[length];
+    memset(tmp, 0, length*sizeof(int));
+    for(int i = 0; i < length; i++) {
+        if(nums[i] >= 0 && nums[i] < length) {
+            tmp[nums[i]] = nums[i];
         }
-        counts++;
     }
-    int i;
-    for(i = 0; i < length; i++) {
-        if(nums[i] >= 0 && nums[i] != i)
-            return i;
+    int j;
+    for(j = 0; j < length; j++) {
+        if(tmp[j] != j)
+            return j;
     }
-    return i;
+    return j;
+    delete[] tmp;
 }
-
-
 
 int main()
 {
     vector<int> nums;
-    nums.push_back(3);
-    nums.push_back(4);
-    nums.push_back(-1);
+//    nums.push_back(3);
+//    nums.push_back(4);
+//    nums.push_back(-1);
+//    nums.push_back(1);
     nums.push_back(1);
+    nums.push_back(2);
+    nums.push_back(0);
     int result = firstMissingPositive(nums);
     cout<<"The result is: "<<result<<endl;
     return 0;
