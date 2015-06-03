d3e20ca34abc30713c0abb637ee0587b0bae6ab2
chang <lchangrwx@gmail.com>
2015-06-03 20:23:48 +0800

    maximum depth of binary tree

---
+++ Tree/maximumDepthOfBinaryTree/main.cpp
@@ -0,0 +1,29 @@
+
+//Maximum Depth of Binary Tree
+
+#include <iostream>
+
+using namespace std;
+
+struct TreeNode {
+    int val;
+    TreeNode* left;
+    TreeNode* right;
+    TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+};
+
+//没有进行测试
+int maxDepth(TreeNode* root)
+{
+    if(root == NULL)
+        return 0;
+    int left = maxDepth(root->left);
+    int right = maxDepth(root->right);
+    return left >= right ? (left + 1) : (right + 1);
+}
+
+int main()
+{
+    cout << "Hello world!" << endl;
+    return 0;
+}
