61c98e53589baecb495ed02a10f7be401010321b
chang <lchangrwx@gmail.com>
2015-06-07 22:44:49 +0800

    add binary

--- String/addBinary/main.cpp
+++ String/addBinary/main.cpp
@@ -3,6 +3,7 @@
 
 #include <iostream>
 #include <stack>
+#include <cmath>
 
 using namespace std;
 
@@ -58,7 +59,30 @@ string addBinary(string a, string b)
 {
     int lengthA = a.size();
     int lengthB = b.size();
-    int length = lengthA <= lengthB ? lengthA : lengthB;
+    int length = abs(lengthA - lengthB);
+    string tmp(length, '0');
+    string result;
+    int flag = 0;
+    if(lengthA > lengthB) {
+        b = tmp + b;
+        result.resize(lengthA, '0');
+    }
+    else {
+        a = tmp + a;
+        result.resize(lengthB, '0');
+    }
+    for(int j = result.size()-1; j >= 0; j--) {
+        int current = (a[j] - '0') ^ (b[j] - '0') ^ flag;
+        if((a[j] - '0') + (b[j] - '0') + flag > 1)
+            flag = 1;
+        else
+            flag = 0;
+        result[j] = current + '0';
+    }
+    if(flag == 1)
+        result = '1' + result;
+
+    return result;
 }
 
 
@@ -66,6 +90,8 @@ int main()
 {
     string stra = "10100000100100110110010000010101111011011001101110111111111101000000101111001110001111100001101";
     string strb = "110101001011101110001111100110001010100001101011101010000011011011001011101111001100000011011110011";
+//    string stra = "111";
+//    string strb = "1";
     string result = addBinary(stra, strb);
 //    string str = "0";
 //    int n = 0;
