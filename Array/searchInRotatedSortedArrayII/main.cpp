66a28f3fe2aa8de248578f3562081cd8a71be14d
chang <lchangrwx@gmail.com>
2015-06-23 20:37:09 +0800

    search in rotated sorted array II

--- Array/searchInRotatedSortedArrayII/main.cpp
+++ Array/searchInRotatedSortedArrayII/main.cpp
@@ -6,41 +6,7 @@
 
 using namespace std;
 
-//int search(vector<int> &nums, int target)
-//{
-//    int length = nums.size();
-//    if(length == 0)
-//        return -1;
-//    int low = 0;
-//    int high = length - 1;
-//    int mid;
-//
-//    while(low <= high) {
-//        mid = low + (high - low) / 2;
-//        if(target == nums[mid])
-//            return mid;
-//        else if(target < nums[mid]) {
-//            if(nums[mid] < nums[high])
-//                high = mid - 1;
-//            else
-//                if(target <= nums[low])
-//                    low = mid + 1;
-//                else
-//                    high = mid - 1;
-//        }
-//        else {
-//            if(nums[mid] > nums[low])
-//                low = mid + 1;
-//            else
-//                if(target <= nums[high])
-//                    low = mid + 1;
-//                else
-//                    high = mid - 1;
-//        }
-//    }
-//    return -1;
-//}
-
+////Search in Rotated Sorted Array I
 //int search(vector<int> &nums, int target)
 //{
 //    int length = nums.size();
@@ -52,35 +18,31 @@ using namespace std;
 //
 //    while(low < high) {
 //        mid = low + (high - low) / 2;
-//        if(target == nums[mid])
+//        if(nums[mid] == target)
 //            return mid;
-//        else if(target < nums[mid]) {
-//            if(nums[mid] < nums[high])
+//        if(nums[low] <= nums[mid]) {
+//            if(target < nums[mid] && target >= nums[low])
 //                high = mid;
 //            else
-//                if(target >= nums[low])
-//                    high = mid;
-//                else
-//                    low = mid + 1;
+//                low = mid + 1;
 //        }
 //        else {
-//            if(nums[mid] > nums[low])
+//            if(target > nums[mid] && target <= nums[high - 1])
 //                low = mid + 1;
 //            else
-//                if(target <= nums[high])
-//                    low = mid + 1;
-//                else
-//                    high = mid;
+//                high = mid;
 //        }
 //    }
 //    return -1;
 //}
 
-int search(vector<int> &nums, int target)
+
+//Search in Rotated Sorted Array II
+bool search(vector<int> &nums, int target)
 {
     int length = nums.size();
     if(length == 0)
-        return -1;
+        return false;
     int low = 0;
     int high = length;
     int mid;
@@ -88,22 +50,30 @@ int search(vector<int> &nums, int target)
     while(low < high) {
         mid = low + (high - low) / 2;
         if(nums[mid] == target)
-            return mid;
-        if(nums[low] <= nums[mid]) {
-            if(target < nums[mid] && target >= nums[low])
+            return true;
+        if(nums[mid] > nums[low]) {
+            if(target < nums[mid] && target >= nums[low]) {
                 high = mid;
-            else
+            }
+            else {
                 low = mid + 1;
+            }
+        }
+        else if(nums[mid] < nums[low]) {
+            if(target > nums[mid] && target <= nums[high - 1]) {
+                low = mid + 1;
+            }
+            else {
+                high = mid;
+            }
         }
         else {
-            if(target > nums[mid] && target <= nums[high - 1])
-                low = mid + 1;
-            else
-                high = mid;
+            low++;
         }
     }
-    return -1;
+    return false;
 }
+
 
 
 vector<int> arrayToVector(int arr[], int n)
@@ -127,11 +97,11 @@ void printVector(vector<int> &nums)
 int main()
 {
 //    int arr[] = {4, 5, 6, 7, 0, 1, 2};
-    int arr[] = {3, 5, 1};
+    int arr[] = {3, 5, 1, 1};
     int n = sizeof(arr)/sizeof(arr[0]);
     vector<int> nums;
     nums = arrayToVector(arr, n);
-    int target = 3;
+    int target = 1;
     int result = search(nums, target);
     cout<<"The result is: "<<result<<endl;
     return 0;
