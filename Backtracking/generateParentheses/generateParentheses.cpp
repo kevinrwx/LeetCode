efa86b944c1bbdb1ec93d59ef5980237b9774653
chang <lchangrwx@gmail.com>
2015-07-29 00:14:37 +0800

    genenrate parentheses

--- Backtracking/generateParentheses/generateParentheses.cpp
+++ Backtracking/generateParentheses/generateParentheses.cpp
@@ -1,5 +1,5 @@
 
-//Generate Parentheses 
+//Generate Parentheses
 
 #include <iostream>
 #include <vector>
@@ -61,6 +61,88 @@ int main()
 {
 	vector<string> results;
 	int n = 1;
+	results = generateParenthesis(n);
+	printVector(results);
+	return 0;
+}
+
+
+
+
+//Generate Parentheses
+
+#include <iostream>
+#include <vector>
+#include <stack>
+#include <string>
+
+using namespace std;
+
+string reverse(string str)
+{
+    string result;
+    int len = str.length();
+    for(int i = len - 1; i >= 0; i--)
+        result.push_back(str[i]);
+    return result;
+}
+
+void generate(vector<string> &results, string tmp, stack<char> st, string bracket, int k)
+{
+	if(k == 0) {
+        if(st.empty()) {
+            string temp = reverse(tmp);
+            results.push_back(temp);
+            tmp.clear();
+        }
+	}
+	else {
+		for(size_t i = 0; i < bracket.length(); i++) {
+			if(bracket[i] == bracket[0]) {
+				st.push(bracket[i]);
+				generate(results, tmp, st, bracket, k-1);
+				st.pop();
+			}
+			else {
+				if(!st.empty()) {
+					tmp.push_back(bracket[i]);
+					char temp = st.top();
+					tmp.push_back(temp);
+					st.pop();
+					generate(results, tmp, st, bracket, k-1);
+					//st.push(temp);
+				}
+			}
+		}
+	}
+}
+
+vector<string> generateParenthesis(int n)
+{
+	vector<string> results;
+	if(n == 0)
+		return results;
+	stack<char> st;
+	string tmp;
+	string bracket = "()";
+	int k = 2 * n;
+	generate(results, tmp, st, bracket, k);
+	return results;
+}
+
+void printVector(vector<string> &results)
+{
+	int len = results.size();
+	if(len == 0)
+		cout<<"empty results"<<endl;
+	for(int i = 0; i < len; i++)
+		cout<<results[i]<<endl;
+}
+
+int main()
+{
+	vector<string> results;
+	int n = 2;
 	results = generateParenthesis(n);
 	printVector(results);
 	return 0;
