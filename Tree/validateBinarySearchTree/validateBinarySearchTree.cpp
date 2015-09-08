2602797784759445f19a1bee00106ecd09e5bd8e
Kevin Liu <lchangrwx@gmail.com>
2015-09-08 08:06:27 +0800

    method III

--- Tree/validateBinarySearchTree/validateBinarySearchTree.cpp
+++ Tree/validateBinarySearchTree/validateBinarySearchTree.cpp
@@ -3,6 +3,7 @@
 
 #include <iostream>
 #include <vector>
+#include <limits.h>
 
 using namespace std;
 
@@ -40,9 +41,29 @@ struct TreeNode {
 
 //Method II
 //Method I方法的改进，可以不需要有保存所有的中序遍历结果
+// bool isValidBST(TreeNode* root)
+// {
+// 	//
+// }
+
+//Method III
+bool isBSTUtil(TreeNode* root, int min, int max)
+{
+	cout<<min<<endl;
+	cout<<max<<endl;
+	if(root == NULL)
+		return true;
+	if(root->val < min || root->val > max)
+		return false;
+
+	return isBSTUtil(root->left, min, root->val-1) && isBSTUtil(root->right, root->val+1, max);
+}
+
 bool isValidBST(TreeNode* root)
 {
-	//
+	long long MIN = -9223372036854775807;
+	long long MAX = 9223372036854775804;
+	return isBSTUtil(root, MIN, MAX);
 }
 
 int main()
