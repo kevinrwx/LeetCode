cd15559f3e03d4a8e51015399336d6b2136c8ca8
Kevin Liu <lchangrwx@gmail.com>
2015-12-10 18:55:59 +0800

    jump games II

--- Array/jumpGameII/jumpGameII.cpp
+++ Array/jumpGameII/jumpGameII.cpp
@@ -6,25 +6,25 @@
 
 using namespace std;
 
+//仍然使用贪心算法，我们维护两个变量，当前能到达的最远点p以及下一次能到达的最远点q，在p的范围内迭代计算q，
+//然后更新步数，并将最大的q设置为p，重复这个过程直到p到达终点
 int jump(vector<int>& nums) {
 	int length = nums.size();
 	int res = 0;
-	if(length <= 0) {
-		return res;
-	}
-	int maxLocal = 0;
-	int maxLocalIndex = 0;
-	for(int i = 0; i < length; i++) {
-		if(maxLocal - i + maxLocalIndex >= 0) {
-			if(nums[i] >= maxLocal && (maxLocal < length - 1 - maxLocalIndex)) {
-				maxLocal = nums[i];
-				maxLocalIndex = i;
-				res++;
-			}
+	int p = 0;
+	int q = 0;
+
+	int i = 0;
+	while(i < length) {
+		if(p >= length - 1)
+			break;
+		while(i <= p) {
+			q = max(q, i + nums[i]);
+			i++;
 		}
+		p = q;
+		res++;
 	}
-	if(maxLocal == nums[length - 1])
-		res--;
 	return res;
 }
 
