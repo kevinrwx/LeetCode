3f1425560d294326b5b81eb57013acf9c12eb469
Kevin Liu <lchangrwx@gmail.com>
2015-12-19 10:49:31 +0800

    nqueensI II

---
+++ Backtracking/nQueens/nQueens.cpp
@@ -0,0 +1,19 @@
+
+//N-Queens
+
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+vector<vector<string> > solveNQueens(int n) {
+	//
+}
+
+void printVector(vector<vector<string> >& result) {
+	//
+}
+
+int main() {
+	//
+}
