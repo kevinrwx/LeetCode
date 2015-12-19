a7f05b366d315c7a0a527dc443d496b2bc7d65ba
Kevin Liu <lchangrwx@gmail.com>
2015-12-20 00:00:49 +0800

    nqueens ii

--- Backtracking/nQueensII/nQueensII.cpp
+++ Backtracking/nQueensII/nQueensII.cpp
@@ -3,16 +3,48 @@
 //N-Queens II
 
 #include <iostream>
+#include <vector>
 
 using namespace std;
 
-int totalNQueens(int n)
-{
-    //
+bool is_collision(vector<string>& tmp, int x, int y) {
+	for(int i = x-1, j = y-1; i >= 0 && j >= 0; i--, j--)
+		if(tmp[i][j] == 'Q')
+			return true;
+	for(int i = x-1, j = y+1; i >= 0 && j < tmp.size(); i--, j++)
+		if(tmp[i][j] == 'Q')
+			return true;
+	return false;
 }
 
-int main()
-{
-    cout << "Hello world!" << endl;
+void backtrack(int* num, vector<string>& tmp, int k, vector<int>& candinates) {
+	if(k == tmp.size()) {
+		(*num) += 1;
+	}
+	else {
+		for(int i = 0; i < tmp.size(); i++) {
+			if(candinates[i] == 1 || is_collision(tmp, k, i))
+				continue;
+			tmp[k][i] = 'Q';
+			candinates[i] = 1;
+			backtrack(num, tmp, k+1, candinates);
+			tmp[k][i] = '.';
+			candinates[i] = 0;
+		}
+	}
+}
+
+int totalNQueens(int n) {
+    int num = 0;
+    vector<string> tmp(n, string(n, '.'));
+    vector<int> candinates(n, 0);
+    backtrack(&num, tmp, 0, candinates);
+    return num;
+}
+
+int main() {
+    int n = 5;
+    int result = totalNQueens(n);
+    cout<<result<<endl;
     return 0;
 }
